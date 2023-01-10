856dfd163746a8494c39356cf33ee574d9aae3f8
Alex Vranas <avranas42@gmail.com>
2023-01-10 15:25:23 -0800

    Modified About Us section on the homepage. Added link to github repo

--- client/src/routes/HomePage/HomePage.js
+++ client/src/routes/HomePage/HomePage.js
@@ -1,9 +1,8 @@
-import CategoryList from '../../components/CategoryList/CategoryList';
-import { Link } from 'react-router-dom';
-import './HomePage.css';
+import CategoryList from "../../components/CategoryList/CategoryList";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 const HomePage = (props) => {
-
   return (
     <main id="home-page">
       <section id="splash">
@@ -11,23 +10,32 @@ const HomePage = (props) => {
       </section>
       <section id="shop-all-products-button-holder">
         <Link to="/products/0">
-          <button className='important-button'>Shop all products</button>
+          <button className="important-button">Shop all products</button>
         </Link>
       </section>
-      <h2 className='container'>Shop By Category</h2>
+      <h2 className="container">Shop By Category</h2>
       <CategoryList />
-      <h2 className='container'>About Us</h2>
-      <section className='container' id="about-us">
-        <p>The Fake Climbing Gear Shop is an e-commerce app made by
-          <a href="https://github.com/avranas"> Alex Vranas</a> to practice
-          building full stack web apps. No climbing gear is actually being sold
-          here, but you can expect this site to do everything that you expect
-          an e-commerce site is capable of.</p>
-        <p>For payments, this site uses Stripe, which is currently set to test
+      <h2 className="container">About Us</h2>
+      <section className="container" id="about-us">
+        <p>
+          The Fake Climbing Gear Shop is an e-commerce app made by{" "}
+          <a href="https://github.com/avranas"> Alex Vranas.</a> It was made by
+          the developer to practice building full stack web apps. This site does
+          not actually sell any climbing gear, but it can do just about
+          everything that an e-commerce app should be capable of.
+        </p>
+        <p>
+          For payments, this site uses Stripe, which is currently set to test
           mode. If you would like to see how payments are processed, enter the
-          credit card number: "4242 4242 4242 4242" with fake data for 
-          everything else.</p>
-       </section>
+          credit card number: "4242 4242 4242 4242" with fake data for
+          everything else.
+        </p>
+        <p>
+          <a href="https://github.com/avranas/The-Fake-Climbing-Gear-Shop">
+            Link to GitHub repository
+          </a>
+        </p>
+      </section>
     </main>
   );
 };
